a90ef7a006c8419a3c5b1c5968ef67262c2f51a4
Dark Amazon <250732274+cyber-security-noida@users.noreply.github.com>
2025-12-30 15:43:55 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'amazon-dark-v1';
+const ASSETS = [
+  './',
+  './index.html', // Assuming your main file is index.html
+  './manifest.json'
+];
+
+// Install Event
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetch Event
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
